015165dd6faddb464988b79dfb3fc986e3a83cd4
TaslimOwolarafe <owolarafetaslim@gmail.com>
2024-07-10 05:51:37 +0100

    add serializers to nested fields

--- competitors/serializers.py
+++ competitors/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
 from .models import Competitor
 
+
+class WebsiteTrafficSerializer(serializers.Serializer):
+    number_of_visitors = serializers.IntegerField()
+    page_views = serializers.IntegerField()
+    unique_visitors = serializers.IntegerField()
+    session_duration = serializers.CharField()
+    bounce_rate = serializers.CharField()
+
+class TopPerformingPageSerializer(serializers.Serializer):
+    page = serializers.CharField()
+    page_views = serializers.IntegerField()
+    unique_visitors = serializers.IntegerField()
+    engagement_rate = serializers.CharField()
+    average_time_on_page = serializers.CharField()
+
 class CompetitorSerializer(serializers.ModelSerializer):
+    website_traffic = WebsiteTrafficSerializer()
+    top_performing_pages = TopPerformingPageSerializer(many=True)
+
     class Meta:
         model = Competitor
-        fields = '__all__'
+        fields = ['business_name', 'business_type', 'location', 'website_traffic', 'top_performing_pages']
